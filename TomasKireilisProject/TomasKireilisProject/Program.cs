51cbe1edda624ba1b9d5247ce1c2981c571a30d2
Tomas <t.kireilis@gmail.com>
2020-07-09 11:11:27 +0300

    Part 1

--- TomasKireilisProject/TomasKireilisProject/Program.cs
+++ TomasKireilisProject/TomasKireilisProject/Program.cs
@@ -1,12 +1,59 @@
 ﻿using System;
+using System.Linq;
 
 namespace TomasKireilisProject
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Uncomment which you want to try;
+            // Task1();
+            Task2();
+        }
+
+        public static void Task1()
+        {
+            var input = Console.ReadLine();
+            //For fast check
+            //  input = "I donn’t knoow pii value,  so I will go eat my ppiie…";
+            input = string.Join(" ",
+                input.Split(' ')
+                    .Select(RemoveLetterRepetitions));
+            input = ReplacePIWithValue(input);
+            Console.WriteLine(input);
+        }
+
+        public static void Task2()
+        {
+        }
+
+        private static string RemoveLetterRepetitions(string input)
+        {
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                if (input[i + 1] == input[i])
+                {
+                    input = input.Remove(i, 1);
+                    i--;
+                }
+            }
+
+            return input;
+        }
+
+        private static string ReplacePIWithValue(string input)
+        {
+            for (int i = 0; i < input.Length - 4; i++)
+            {
+                if (input.Substring(i, 4).ToLower() == " pi ")
+                {
+                    input = input.Remove(i, 4);
+                    input = input.Insert(i, " 3.14 ");
+                }
+            }
+
+            return input;
         }
     }
 }
